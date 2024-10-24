d8f8a5bbd93e136c6b8f373e639e8a1b93f5c023
irfnhanif <14.irfanh@gmail.com>
2024-10-24 07:58:39 +0700

    Refactor footer to update font size in Footer.scss

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -10,10 +10,7 @@ export default function Footer() {
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          {emoji("Made with ❤️ by DeveloperFolio Team")}
-        </p>
-        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          Theme by{" "}
+          {emoji("Made with 🔥 and ☕")} | Theme by{" "}
           <a
             href="https://github.com/saadpasta/developerFolio"
             target="_blank"
